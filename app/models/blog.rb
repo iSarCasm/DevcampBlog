2982d341fbc60fcf510df778932022c59d1bab6d
SarCasm <sarcasm008@gmail.com>
2017-03-27 21:16:45 +0300

    Created topics

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,9 @@
 class Blog < ApplicationRecord
-  enum status: {
-    draft: 0,
-    published: 1
-  }
-
+  enum status: { draft: 0,published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  belongs_to :topic
 
   validates_presence_of :title, :body
 
